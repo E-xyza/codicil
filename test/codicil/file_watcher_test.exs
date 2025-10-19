80021ed304cca637caac7107c41da5e2e7743a5a
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2025-10-19 00:43:08 -0500

    Simplify FileWatcher test by sending messages directly
    Instead of creating/deleting real files, just send file_event messages
    directly to the FileWatcher process. Much simpler and faster.

--- test/codicil/file_watcher_test.exs
+++ test/codicil/file_watcher_test.exs
@@ -12,69 +12,69 @@ defmodule Codicil.FileWatcherTest do
     # Allow background processes (file watcher) to use the sandbox
     Ecto.Adapters.SQL.Sandbox.mode(Repo, {:shared, self()})
 
-    # Enable tracer for this test
-    Code.put_compiler_option(:tracers, [Codicil.Tracer])
-
-    on_exit(fn ->
-      # Disable tracer after test
-      Code.put_compiler_option(:tracers, [])
-    end)
-
     :ok
   end
 
-  # Helper to clean up module and its beam file
-  defp cleanup_module(module) do
-    :code.purge(module)
-    :code.delete(module)
-
-    # Delete beam file if it exists
-    beam_dir = Path.join([File.cwd!(), "test", "_support", "beamfiles"])
-    beam_file = Path.join(beam_dir, "#{module}.beam")
-    File.rm(beam_file)
-  end
-
   describe "module retirement on file deletion" do
-    test "removes module and its functions when source file is deleted" do
-      # Create a temporary module file in test directory
-      test_dir = Path.join([File.cwd!(), "test", "_support", "file_watcher_test"])
-      File.mkdir_p!(test_dir)
+    test "removes module and its functions when file deleted event received" do
+      # Create a module record
+      test_path = "/lib/test_module.ex"
 
-      test_module_path = Path.join(test_dir, "file_watcher_test_module.ex")
+      {:ok, _module_record} =
+        Modules.upsert(%{
+          id: "Elixir.TestModule",
+          path: test_path,
+          checksum: "abc123"
+        })
 
-      File.write!(test_module_path, """
-      defmodule FileWatcherTestModule do
-        def test_function(x), do: x + 1
-      end
-      """)
+      # Create a function for this module
+      {:ok, _function} =
+        Functions.upsert(%{
+          name: :test_function,
+          module: TestModule,
+          arity: 1,
+          exported: true,
+          path: test_path,
+          line: 10,
+          checksum: "def456"
+        })
 
-      # Compile the module
-      [{module, _}] = Code.compile_file(test_module_path)
+      # Verify module and function exist
+      assert Modules.get(TestModule)
+      assert Functions.get_by_mfa({TestModule, :test_function, 1})
 
-      # Give the background task time to complete
+      # Send file deletion event to FileWatcher
+      send(Codicil.FileWatcher, {:file_event, self(), {test_path, [:deleted]}})
+
+      # Give FileWatcher time to process
       Process.sleep(100)
 
-      # Verify module and function were created
-      assert module_record = Modules.get(FileWatcherTestModule)
-      assert module_record.id == "Elixir.FileWatcherTestModule"
-      assert function = Functions.get_by_mfa({FileWatcherTestModule, :test_function, 1})
-      assert function.name == "test_function"
-
-      # Delete the source file
-      File.rm!(test_module_path)
-
-      # Give file watcher time to detect deletion and process
-      Process.sleep(500)
-
       # Verify module was removed
-      refute Modules.get(FileWatcherTestModule)
+      refute Modules.get(TestModule)
 
       # Verify function was removed
-      refute Functions.get_by_mfa({FileWatcherTestModule, :test_function, 1})
+      refute Functions.get_by_mfa({TestModule, :test_function, 1})
+    end
 
-      # Clean up
-      cleanup_module(module)
-      File.rm_rf!(test_dir)
+    test "ignores non-elixir file deletions" do
+      # Create a module record
+      test_path = "/lib/test_module.ex"
+
+      {:ok, _module_record} =
+        Modules.upsert(%{
+          id: "Elixir.TestModule2",
+          path: test_path,
+          checksum: "abc123"
+        })
+
+      # Send deletion event for a non-elixir file
+      send(Codicil.FileWatcher, {:file_event, self(), {"/lib/readme.md", [:deleted]}})
+
+      # Give FileWatcher time to process
+      Process.sleep(100)
+
+      # Verify module still exists (not deleted)
+      assert Modules.get(TestModule2)
     end
   end
 end
