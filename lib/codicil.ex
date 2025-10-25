5e984840d0a1309fe7903835d30dd3565f7b6473
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2025-10-25 12:31:18 -0500

    code cleanup

--- lib/codicil.ex
+++ lib/codicil.ex
@@ -2,17 +2,4 @@ defmodule Codicil do
   @moduledoc """
   Documentation for `Codicil`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Codicil.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
